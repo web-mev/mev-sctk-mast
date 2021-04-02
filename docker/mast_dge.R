4d19c19ace4c64f309737b5ccd2b0a446f0428b6
blawney <blawney@hsph.harvard.edu>
2021-04-02 13:44:45 -0400

    Checking in. Not ready yet

--- docker/mast_dge.R
+++ docker/mast_dge.R
@@ -1,25 +1,80 @@
 suppressMessages(suppressWarnings(library("singleCellTK")))
 suppressMessages(suppressWarnings(library("Matrix")))
+suppressMessages(suppressWarnings(library("optparse")))
 
 # args from command line:
 args<-commandArgs(TRUE)
 RAW_COUNT_MATRIX<-args[1]
-BASE_CONDITION_SAMPLES <- args[2]
-EXPERIMENTAL_CONDITION_SAMPLES <- args[3]
-CONDITION_A<-args[4]
-CONDITION_B<-args[5]
+EXPERIMENTAL_CONDITION_SAMPLES <- args[2]
+if (length(args) == 3) {
+    BASE_CONDITION_SAMPLES <- args[3]
+} else {
+    BASE_CONDITION_SAMPLES <- NULL
+}
 OUTPUT_MAST_FILE_BASE <- 'mast_results'
 
-# I split this from the other conditional later so that we can get the checks 
-# out of the way before the time intensive import of data, etc.
-if (CONDITION_B == "FAKE") {# some conditional for the 1-vs-all = TRUE
-    CONDITION_A <- "background" # overwrite / define the CONDITION_A
-    contrast_str = paste0(CONDITION_B, '_vs_', CONDITION_A)
-} else { # 
+option_list <- list(
+  make_option(
+    c('-a','--experimental_samples'),
+    help='The experimental samples, provided as a comma-delimited string.'
+  ),
+  make_option(
+    c('-b','--base_samples'),
+    help='The base samples, provided as a comma-delimited string. Optional.'
+  ),
+  make_option(
+    c('--experimental_group_name'),
+    help='The name of the experimental sample group'
+  ),
+  make_option(
+    c('--base_group_name'),
+    help='The name of the base sample group. Optional, and only relevant if --base_samples given.'
+  )
+)
+
+opt <- parse_args(OptionParser(option_list=option_list))
+
+# No matter what, we need at least the name of the experimental group and which samples
+# are in that group. 
+EXP_CONDITION_NAME <- opt$experimental_group_name
+if (is.null(EXP_CONDITION_NAME)){
+    message('Improper call. Need to specify the name of the experimental group via the --experimental_group_name arg.')
+    quit(status=1)
+}
+EXPERIMENTAL_CONDITION_SAMPLES <- opt$experimental_samples
+if (is.null(EXPERIMENTAL_CONDITION_SAMPLES)){
+    message('Improper call. Need to specify the names of the samples in the group of interest.')
+    quit(status=1)
+}
+exp_samples <- make.names(strsplit(EXPERIMENTAL_CONDITION_SAMPLES, ',')[[1]])
+
+if (is.null(opt$base_samples)) {
+
+    # a flag for later use
+    ONE_VS_ALL = TRUE
+
+    # Creates a 1-vs-all contrast
+    contrast_str = paste0(EXP_CONDITION_NAME, '_vs_all')
+    exp_samples <- make.names(strsplit(EXPERIMENTAL_CONDITION_SAMPLES, ',')[[1]])
+    BASE_CONDITION_NAME <- 'other'
+
+} else { 
+
+    # a flag for later use
+    ONE_VS_ALL = FALSE
+
     # create a string to identify the contrast:
-    contrast_str = paste0(CONDITION_B, '_vs_', CONDITION_A)
+    BASE_CONDITION_NAME <- opt$base_group_name
+    if (is.null(BASE_CONDITION_NAME)){
+        message('Improper call. Need to specify the name of the base group via the --base_group_name arg.')
+        quit(status=1)
+    }
 
+    contrast_str = paste0(EXP_CONDITION_NAME, '_vs_', BASE_CONDITION_NAME)
 
+    # to enter this block, we had to have opt$base_samples as non-null, so don't check again
+    BASE_CONDITION_SAMPLES <- opt$base_samples
+    
     # the sample names are given as a comma-delimited string. Split them
     base_samples <- make.names(strsplit(BASE_CONDITION_SAMPLES, ',')[[1]])
     exp_samples <- make.names(strsplit(EXPERIMENTAL_CONDITION_SAMPLES, ',')[[1]])
@@ -35,6 +90,9 @@ if (CONDITION_B == "FAKE") {# some conditional for the 1-vs-all = TRUE
         ))
         quit(status=1)
     }
+
+    # a list of all the samples we need. Note that these could be completely bogus and
+    # not correspond to anything in the matrix.
     all_samples <- c(base_samples, exp_samples)
 }
 
@@ -45,6 +103,33 @@ cnts <- read.table(
     sep = "\t",
     row.names = 1
 )
+
+# ensure the sample names are kosher and subset the matrix if necessary.
+if (ONE_VS_ALL) {
+    # only need to check the experimental samples in this case.
+    check_samples = exp_samples
+} else {
+    check_samples = all_samples
+}
+ 
+# check the set difference. If there are samples in `check_samples` 
+# that are NOT columns of the matrix, then error out immediately.
+sd <- setdiff(check_samples, colnames(cnts))
+if ( length(sd) > 0 ){
+    sample_list = paste0(sd, collapse=',')
+    message(paste(
+        'The following samples were not in your matrix: ',
+        sample_list
+    ))
+    quit(status=1)
+}
+
+# If we are doing a A vs. B contrast, subset to cut down the matrix
+if (!ONE_VS_ALL){
+    cnts = cnts[,all_samples]
+}
+
+# change to a sparse matrix representation, necessary for SCE
 cnts <- as(as.matrix(cnts), "sparseMatrix")
 
 # Create an SCE object from the counts
@@ -60,26 +145,24 @@ sce <- seuratSCTransform(
 )
 
 # Create the labels list
-if (CONDITION_B == "FAKE") { # some conditional for the 1-vs-all = TRUE
+if (ONE_VS_ALL) {
     labels <- c()
     # A more elegant solution should be implemented.
     # This is "slow".
     for (sample in colnames(sce)) {
         if (sample %in% exp_samples) {
-            labels <- c(labels, CONDITION_B)
+            labels <- c(labels, EXP_CONDITION_NAME)
         } else {
-            labels <- c(labels, CONDITION_A)
+            labels <- c(labels, BASE_CONDITION_NAME)
         }
     }
 } else { # else, do a-vs-b differential expression
     labels <- c()
     for (sample in colnames(sce)) {
         if (sample %in% exp_samples) {
-            labels <- c(labels, CONDITION_B)
-        } else if (sample %in% base_samples) {
-            labels <- c(labels, CONDITION_A)
-        } else {
-            labels <- c(labels, "bobby_drop_tables") # We need some protected NULL type here
+            labels <- c(labels, EXP_CONDITION_NAME)
+        } else (sample %in% base_samples) {
+            labels <- c(labels, BASE_CONDITION_NAME)
         }
     }
 }
@@ -93,8 +176,8 @@ sce <- runDEAnalysis(
     method = "MAST", 
     useAssay = "SCTCounts",
     class = "label", 
-    classGroup1 = CONDITION_B, 
-    classGroup2 = CONDITION_A,
+    classGroup1 = EXP_CONDITION_NAME, 
+    classGroup2 = BASE_CONDITION_NAME,
     groupName1 = "exp", 
     groupName2 = "base", 
     analysisName = "exp_VS_base",
