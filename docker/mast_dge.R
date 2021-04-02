3c41ff9bbf5713ed130818d954c9ecbb7a91d255
blawney <blawney@hsph.harvard.edu>
2021-04-02 15:28:23 -0400

    Adding wdl script. untested

--- docker/mast_dge.R
+++ docker/mast_dge.R
@@ -4,16 +4,16 @@ suppressMessages(suppressWarnings(library("optparse")))
 
 # args from command line:
 args<-commandArgs(TRUE)
-RAW_COUNT_MATRIX<-args[1]
-EXPERIMENTAL_CONDITION_SAMPLES <- args[2]
-if (length(args) == 3) {
-    BASE_CONDITION_SAMPLES <- args[3]
-} else {
-    BASE_CONDITION_SAMPLES <- NULL
-}
-OUTPUT_MAST_FILE_BASE <- 'mast_results'
 
 option_list <- list(
+  make_option(
+    c('-f','--input_file'),
+    help='Path to the count matrix input.'
+  ),
+  make_option(
+    c('-o','--output_file_prefix'),
+    help='The prefix for the output file'
+  ),
   make_option(
     c('-a','--experimental_samples'),
     help='The experimental samples, provided as a comma-delimited string.'
@@ -33,6 +33,17 @@ option_list <- list(
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
+
+# Check that the file was provided:
+if (is.null(opt$input_file)){
+    message('Need to provide a count matrix with the -f/--input_file arg.')
+    quit(status=1)
+}
+
+if (is.null(opt$output_file_prefix)) {
+    message('Need to provide the prefix for the output file with the -o arg.')
+    quit(status=1)
+}
 
 # No matter what, we need at least the name of the experimental group and which samples
 # are in that group. 
@@ -99,7 +110,7 @@ if (is.null(opt$base_samples)) {
 
 # Import counts as a data.frame
 cnts <- read.table(
-    file = RAW_COUNT_MATRIX,
+    file = opt$input_file,
     sep = "\t",
     row.names = 1
 )
@@ -161,7 +172,7 @@ if (ONE_VS_ALL) {
     for (sample in colnames(sce)) {
         if (sample %in% exp_samples) {
             labels <- c(labels, EXP_CONDITION_NAME)
-        } else (sample %in% base_samples) {
+        } else {
             labels <- c(labels, BASE_CONDITION_NAME)
         }
     }
@@ -189,7 +200,7 @@ sce <- runDEAnalysis(
 df.results <- metadata(sce)$diffExp$exp_VS_base$result
 
 # Write results to file
-output_filename <- paste(OUTPUT_MAST_FILE_BASE, contrast_str, 'tsv', sep='.')
+output_filename <- paste($output_file_prefix, contrast_str, 'tsv', sep='.')
 write.table(
     df.results, 
     output_filename, 
---
+++ main.wdl
@@ -0,0 +1,65 @@
+workflow SctkSeuratCluster {
+    
+    # An integer matrix of counts
+    File raw_counts
+
+    # The samples/cells in one group (required)
+    Array[String] expSamples
+
+    # The samples/cells in the other group.
+    # Not required. If missing, then runs a one-vs-all
+    Array[String]? baseSamples
+
+    # a name assigned to the "experimental" group. Required
+    String expGroupName
+
+    # If we are performing an explicit group comparison, 
+    # we require this.
+    String? baseGroupName
+
+    call runMast {
+        input:
+            raw_counts = raw_counts,
+            expSamples = expSamples,
+            baseSamples = baseSamples,
+            expGroupName = expGroupName,
+            baseGroupName = baseGroupName
+    }
+
+    output {
+        File mast_output = runMast.fout
+    }
+}
+
+task runMast {
+    File raw_counts
+    Array[String] expSamples
+    Array[String]? baseSamples
+    String expGroupName
+    String? baseGroupName
+
+    String output_name_prefix = ""
+    Int disk_size = 20
+
+    command {
+        Rscript /opt/software/mast_dge.R \
+            -f ${raw_counts} \
+            -o ${output_name_prefix} \
+            -a ${sep="," expSamples} \
+            ${prefix="-b " sep="," baseSamples} \
+            --experimental_group_name ${expGroupName} \
+            ${"--base_group_name" + baseGroupName}
+    }
+
+    output {
+        File fout = glob("${output_name_prefix}*")[0]
+    }
+
+    runtime {
+        docker: "hsphqbrc/mev-sctk-mast-dge"
+        cpu: 2
+        memory: "12 G"
+        disks: "local-disk " + disk_size + " HDD"
+        preemptible: 0
+    }
+}
